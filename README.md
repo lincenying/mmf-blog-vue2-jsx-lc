012004c673d9f9d9f17d87511878b0769ca28396
lincenying <lincenying@qq.com>
2016-08-20 21:56:43 +0800

    first update

---
+++ README.md
@@ -0,0 +1,34 @@
+# mmf-blog vuejs 2.0 jsx语法版
+
+demo: [http://www.mmxiaowu.com](http://www.mmxiaowu.com)
+
+react: [https://github.com/lincenying/mmf-blog-react](https://github.com/lincenying/mmf-blog-react)
+
+vue1: [https://github.com/lincenying/mmf-blog-vue](https://github.com/lincenying/mmf-blog-vue)
+
+vue2-template: [https://github.com/lincenying/mmf-blog-vue2](https://github.com/lincenying/mmf-blog-vue2)
+
+vue2-jsx: [https://github.com/lincenying/mmf-blog-vue2-jsx](https://github.com/lincenying/mmf-blog-vue2-jsx)
+
+vue2-template 服务端渲染: [https://github.com/lincenying/mmf-blog-vue2-ssr](https://github.com/lincenying/mmf-blog-vue2-ssr) // 未完成
+
+---
+
+先安装 api server: https://github.com/lincenying/mmf-blog-api
+
+```
+// 安装依赖
+npm install
+
+// 生产环境
+npm run build
+
+// 开发环境
+npm run dev
+```
+
+首页
+http://localhost:8080
+
+登录
+http://localhost:8080/login.html
---
+++ src/components/post.jsx
@@ -0,0 +1,96 @@
+/* global window, editormd, testEditor */
+import api from '../api'
+export default {
+    data () {
+        return {
+            editors: null,
+            form: {
+                title: '',
+                category: '',
+                content: ''
+            }
+        }
+    },
+    methods: {
+        handleChange(type, e) {
+            this.form[type] = e.target.value
+        },
+        onSubmit(e) {
+            e.preventDefault()
+            if (this.form.title === '') {
+                this.$store.dispatch('showMsg', '请输入标题')
+            } else if (this.form.category === '') {
+                this.$store.dispatch('showMsg', '请选择分类')
+            } else if ($("#editor").val() === '') {
+                this.$store.dispatch('showMsg', '请输入内容')
+            } else {
+                api.postTopic({
+                    title: this.form.title,
+                    category: this.form.category,
+                    content: window.postEditor.getHTML()
+                }).then( () => {
+                    this.$store.commit('DELETE_ADMIN_TOPICS_PATH')
+                    this.$store.dispatch('showMsg', {
+                        content: '发布成功',
+                        type: "success"
+                    })
+                    $("#article-post").get(0).reset()
+                    window.postEditor.clear()
+                }, err => {
+                    this.$store.dispatch('showMsg', err.toString())
+                })
+            }
+        }
+    },
+    mounted() {
+        window.postEditor = editormd("post-content", {
+            width: "100%",
+            height: 500,
+            markdown: "",
+            placeholder: '请输入内容...',
+            path: './static/editor.md/lib/',
+            toolbarIcons() {
+                return [
+                    "bold", "italic", "quote", "|",
+                    "list-ul", "list-ol", "hr", "|",
+                    "link", "reference-link", "image", "code", "code-block", "table", "|",
+                    "watch", "preview", "fullscreen", "|",
+                    "help"
+                ]
+            },
+            watch : false,
+            saveHTMLToTextarea : true,
+            imageUpload : true,
+            imageFormats : ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
+            imageUploadURL : "/api/?action=upload"
+        })
+        this.$store.dispatch('gProgress', 100)
+    },
+    render(h) { // eslint-disable-line
+        return (
+            <div class="g-mn">
+                <div class="box">
+                    <form id="article-post" action="#" method="post">
+                        <section id="post-title">
+                            <input on-change={this.handleChange.bind(this, 'title')} type="text" name="title" class="form-control" placeholder="请输入标题" />
+                        </section>
+                        <section id="post-category">
+                            <select on-change={this.handleChange.bind(this, 'category')} id="category" name="category" class="form-control">
+                                <option value="">请选择分类</option>
+                                <option value="1">生活</option>
+                                <option value="2">工作</option>
+                                <option value="3">其他</option>
+                            </select>
+                        </section>
+                        <section id="post-content">
+                            <textarea id="editor" name="content" class="form-control hidden" data-autosave="editor-content"></textarea>
+                        </section>
+                        <section id="post-submit">
+                            <button on-click={this.onSubmit} class="btn btn-success">发布</button>
+                        </section>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
