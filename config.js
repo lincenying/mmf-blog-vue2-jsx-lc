f7b74c39e8a38d017d1ba2902598d6d8a611e54a
lincenying <lincenying@qq.com>
2016-09-02 21:28:20 +0800

    *

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
         index: path.resolve(__dirname, 'dist/index.html'),
         assetsRoot: path.resolve(__dirname, 'dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: './',
+        assetsPublicPath: '/mmf-blog-vue2-jsx-lc/',
         productionSourceMap: false
     },
     dev: {
